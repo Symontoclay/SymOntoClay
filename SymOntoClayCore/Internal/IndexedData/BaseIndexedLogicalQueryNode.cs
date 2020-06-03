c81f656e696dbd060c33fc9fef651aa8dba1ec11
sergiy.tolkachov <sergiy.tolkachov@chainbalance.com>
2020-06-03 12:12:50 +0300

    snapshot

--- SymOntoClayCore/Internal/IndexedData/BaseIndexedLogicalQueryNode.cs
+++ SymOntoClayCore/Internal/IndexedData/BaseIndexedLogicalQueryNode.cs
@@ -1,4 +1,5 @@
 ﻿using SymOntoClay.Core.Internal.CodeModel;
+using SymOntoClay.CoreHelper.DebugHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +10,38 @@ namespace SymOntoClay.Core.Internal.IndexedData
     {
         public abstract KindOfLogicalQueryNode Kind { get; }
         public abstract KindOfOperatorOfLogicalQueryNode KindOfOperator { get; }
+
+        /// <inheritdoc/>
+        protected override string PropertiesToString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(Kind)} = {Kind}");
+            sb.AppendLine($"{spaces}{nameof(KindOfOperator)} = {KindOfOperator}");
+            sb.Append(base.PropertiesToString(n));
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        protected override string PropertiesToShortString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(Kind)} = {Kind}");
+            sb.AppendLine($"{spaces}{nameof(KindOfOperator)} = {KindOfOperator}");
+            sb.Append(base.PropertiesToShortString(n));
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        protected override string PropertiesToBriefString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(Kind)} = {Kind}");
+            sb.AppendLine($"{spaces}{nameof(KindOfOperator)} = {KindOfOperator}");
+            sb.Append(base.PropertiesToBriefString(n));
+            return sb.ToString();
+        }
     }
 }

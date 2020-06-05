598299c5ee1342f1006a3fbe8f9e5b4267ac715f
sergiy.tolkachov <sergiy.tolkachov@chainbalance.com>
2020-06-05 10:16:57 +0300

    snapshot

--- SymOntoClayCore/Internal/IndexedData/IndexedBaseRulePart.cs
+++ SymOntoClayCore/Internal/IndexedData/IndexedBaseRulePart.cs
@@ -13,15 +13,48 @@ namespace SymOntoClay.Core.Internal.IndexedData
         public IndexedRuleInstance Parent { get; set; }
         public BaseIndexedLogicalQueryNode Expression { get; set; }
 
+        public bool IsActive { get; set; }
+        public bool HasVars { get; set; }
+        public bool HasQuestionVars { get; set; }
+
+        public IDictionary<ulong, IList<RelationIndexedLogicalQueryNode>> RelationsDict { get; set; }
+
         /// <inheritdoc/>
         protected override string PropertiesToString(uint n)
         {
             var spaces = DisplayHelper.Spaces(n);
             var sb = new StringBuilder();
+            var nextN = n + 4;
+            var nextNSpace = DisplayHelper.Spaces(nextN);
+
+            sb.AppendLine($"{spaces}{nameof(IsActive)} = {IsActive}");
+            sb.AppendLine($"{spaces}{nameof(HasVars)} = {HasVars}");
+            sb.AppendLine($"{spaces}{nameof(HasQuestionVars)} = {HasQuestionVars}");
 
             sb.PrintBriefObjProp(n, nameof(OriginRulePart), OriginRulePart);
             sb.PrintBriefObjProp(n, nameof(Parent), Parent);
             sb.PrintObjProp(n, nameof(Expression), Expression);
+
+            if (RelationsDict == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(RelationsDict)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(RelationsDict)}");
+                var nextNextN = nextN + 4;
+                foreach (var relationsKVPItem in RelationsDict)
+                {
+                    sb.AppendLine($"{nextNSpace}key of relation = {relationsKVPItem.Key}");
+                    var tmpRelationsList = relationsKVPItem.Value;
+                    sb.AppendLine($"{nextNSpace}count of relations = {tmpRelationsList.Count}");
+                    foreach (var relation in tmpRelationsList)
+                    {
+                        sb.Append(relation.ToShortString(nextNextN));
+                    }
+                }
+                sb.AppendLine($"{spaces}End {nameof(RelationsDict)}");
+            }
 
             sb.Append(base.PropertiesToString(n));
             return sb.ToString();
@@ -32,6 +65,10 @@ namespace SymOntoClay.Core.Internal.IndexedData
         {
             var spaces = DisplayHelper.Spaces(n);
             var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(IsActive)} = {IsActive}");
+            sb.AppendLine($"{spaces}{nameof(HasVars)} = {HasVars}");
+            sb.AppendLine($"{spaces}{nameof(HasQuestionVars)} = {HasQuestionVars}");
 
             sb.PrintBriefObjProp(n, nameof(OriginRulePart), OriginRulePart);
             sb.PrintBriefObjProp(n, nameof(Parent), Parent);
@@ -46,6 +83,10 @@ namespace SymOntoClay.Core.Internal.IndexedData
         {
             var spaces = DisplayHelper.Spaces(n);
             var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(IsActive)} = {IsActive}");
+            sb.AppendLine($"{spaces}{nameof(HasVars)} = {HasVars}");
+            sb.AppendLine($"{spaces}{nameof(HasQuestionVars)} = {HasQuestionVars}");
 
             sb.PrintBriefObjProp(n, nameof(OriginRulePart), OriginRulePart);
             sb.PrintBriefObjProp(n, nameof(Parent), Parent);

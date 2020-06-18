cb542d9e94164a37d80ea36dfd9a5edab3e9c0d7
sergiy.tolkachov <sergiy.tolkachov@chainbalance.com>
2020-06-18 08:29:56 +0300

    snapshot

---
+++ SymOntoClayCore/Internal/IndexedData/ScriptingData/OperationCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.Core.Internal.IndexedData.ScriptingData
+{
+    public enum OperationCode
+    {
+        /// <summary>
+        /// Does nothing.
+        /// </summary>
+        Nop
+    }
+}
---
+++ SymOntoClayCore/Internal/IndexedData/ScriptingData/ScriptCommand.cs
@@ -0,0 +1,84 @@
+﻿using SymOntoClay.CoreHelper.DebugHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.Core.Internal.IndexedData.ScriptingData
+{
+    public class ScriptCommand : IObjectToString, IObjectToShortString, IObjectToBriefString
+    {
+        public OperationCode OperationCode { get; set; } = OperationCode.Nop;
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return ToString(0u);
+        }
+
+        /// <inheritdoc/>
+        public string ToString(uint n)
+        {
+            return this.GetDefaultToStringInformation(n);
+        }
+
+        /// <inheritdoc/>
+        string IObjectToString.PropertiesToString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(OperationCode)} = {OperationCode}");
+
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        public string ToShortString()
+        {
+            return ToShortString(0u);
+        }
+
+        /// <inheritdoc/>
+        public string ToShortString(uint n)
+        {
+            return this.GetDefaultToShortStringInformation(n);
+        }
+
+        /// <inheritdoc/>
+        string IObjectToShortString.PropertiesToShortString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(OperationCode)} = {OperationCode}");
+
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        public string ToBriefString()
+        {
+            return ToBriefString(0u);
+        }
+
+        /// <inheritdoc/>
+        public string ToBriefString(uint n)
+        {
+            return this.GetDefaultToBriefStringInformation(n);
+        }
+
+        /// <inheritdoc/>
+        string IObjectToBriefString.PropertiesToBriefString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(OperationCode)} = {OperationCode}");
+
+            return sb.ToString();
+        }
+    }
+}

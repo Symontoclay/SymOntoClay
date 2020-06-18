552b5361bc603949aecace0e6e041725810eb096
sergiy.tolkachov <sergiy.tolkachov@chainbalance.com>
2020-06-18 12:21:33 +0300

    snapshot

---
+++ SymOntoClayCore/Internal/IndexedData/ScriptingData/CompiledFunctionBody.cs
@@ -0,0 +1,97 @@
+﻿using SymOntoClay.CoreHelper.DebugHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.Core.Internal.IndexedData.ScriptingData
+{
+    public class CompiledFunctionBody : IObjectToString, IObjectToShortString, IObjectToBriefString
+    {
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return ToString(0u);
+        }
+
+        /// <inheritdoc/>
+        public string ToString(uint n)
+        {
+            return this.GetDefaultToStringInformation(n);
+        }
+
+        /// <inheritdoc/>
+        string IObjectToString.PropertiesToString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(OperationCode)} = {OperationCode}");
+            sb.AppendLine($"{spaces}{nameof(Position)} = {Position}");
+            sb.AppendLine($"{spaces}{nameof(Value)} = {Value}");
+            sb.AppendLine($"{spaces}{nameof(Name)} = {Name}");
+            sb.AppendLine($"{spaces}{nameof(JumpTo)} = {JumpTo}");
+            sb.AppendLine($"{spaces}{nameof(KindOfOperator)} = {KindOfOperator}");
+
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        public string ToShortString()
+        {
+            return ToShortString(0u);
+        }
+
+        /// <inheritdoc/>
+        public string ToShortString(uint n)
+        {
+            return this.GetDefaultToShortStringInformation(n);
+        }
+
+        /// <inheritdoc/>
+        string IObjectToShortString.PropertiesToShortString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(OperationCode)} = {OperationCode}");
+            sb.AppendLine($"{spaces}{nameof(Position)} = {Position}");
+            sb.AppendLine($"{spaces}{nameof(Value)} = {Value}");
+            sb.AppendLine($"{spaces}{nameof(Name)} = {Name}");
+            sb.AppendLine($"{spaces}{nameof(JumpTo)} = {JumpTo}");
+            sb.AppendLine($"{spaces}{nameof(KindOfOperator)} = {KindOfOperator}");
+
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        public string ToBriefString()
+        {
+            return ToBriefString(0u);
+        }
+
+        /// <inheritdoc/>
+        public string ToBriefString(uint n)
+        {
+            return this.GetDefaultToBriefStringInformation(n);
+        }
+
+        /// <inheritdoc/>
+        string IObjectToBriefString.PropertiesToBriefString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(OperationCode)} = {OperationCode}");
+            sb.AppendLine($"{spaces}{nameof(Position)} = {Position}");
+            sb.AppendLine($"{spaces}{nameof(Value)} = {Value}");
+            sb.AppendLine($"{spaces}{nameof(Name)} = {Name}");
+            sb.AppendLine($"{spaces}{nameof(JumpTo)} = {JumpTo}");
+            sb.AppendLine($"{spaces}{nameof(KindOfOperator)} = {KindOfOperator}");
+
+            return sb.ToString();
+        }
+    }
+}

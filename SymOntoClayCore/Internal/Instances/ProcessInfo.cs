213293325a5bf1b184c2fba5bedcbee9a602aaf8
metatypeman <metatypeman@gmail.com>
2020-10-09 13:25:29 +0300

    sprint 7 changing version

--- SymOntoClayCore/Internal/Instances/ProcessInfo.cs
+++ SymOntoClayCore/Internal/Instances/ProcessInfo.cs
@@ -1,4 +1,14 @@
-﻿using SymOntoClay.Core.Internal.CodeExecution;
+/*Copyright (C) 2020 Sergiy Tolkachov aka metatypeman
+
+This file is part of SymOntoClay.
+
+SymOntoClay is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; version 2.1.
+
+SymOntoClay is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library; if not, see <https://www.gnu.org/licenses/>*/
+
+using SymOntoClay.Core.Internal.CodeExecution;
 using SymOntoClay.Core.Internal.CodeModel.Helpers;
 using SymOntoClay.CoreHelper.DebugHelpers;
 using System;

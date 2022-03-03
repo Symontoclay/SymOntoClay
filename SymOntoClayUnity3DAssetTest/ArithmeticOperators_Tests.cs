6051a51ce0afb96efaab4ab9d4382b2ce18cefcc
metatypeman <metatypeman@gmail.com>
2022-03-03 18:01:20 +0200

    snapshot

--- SymOntoClayUnity3DAssetTest/ArithmeticOperators_Tests.cs
+++ SymOntoClayUnity3DAssetTest/ArithmeticOperators_Tests.cs
@@ -203,5 +203,81 @@ private:
                     }
                 }), true);
         }
+
+        [Test]
+        [Parallelizable]
+        public void Case2_a()
+        {
+            var text = @"app PeaceKeeper
+{
+private:
+    on Init =>
+    {
+        'Begin' >> @>log;
+        3 - -1 >> @>log;
+        'End' >> @>log;
+    }
+}";
+
+            Assert.AreEqual(BehaviorTestEngineInstance.Run(text,
+                (n, message) =>
+                {
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual(message, "Begin");
+                            break;
+
+                        case 2:
+                            Assert.AreEqual(message, "4");
+                            break;
+
+                        case 3:
+                            Assert.AreEqual(message, "End");
+                            break;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }), true);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void Case2_b()
+        {
+            var text = @"app PeaceKeeper
+{
+private:
+    on Init =>
+    {
+        'Begin' >> @>log;
+        -3 - 1 >> @>log;
+        'End' >> @>log;
+    }
+}";
+
+            Assert.AreEqual(BehaviorTestEngineInstance.Run(text,
+                (n, message) =>
+                {
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual(message, "Begin");
+                            break;
+
+                        case 2:
+                            Assert.AreEqual(message, "-4");
+                            break;
+
+                        case 3:
+                            Assert.AreEqual(message, "End");
+                            break;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }), true);
+        }
     }
 }

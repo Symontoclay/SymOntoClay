fe29f26a225d2ee14f61fd23d4962f8b12f4cef7
metatypeman <metatypeman@gmail.com>
2022-03-06 16:54:49 +0200

    snapshot

---
+++ SymOntoClayUnity3DAssetTest/If_else_elif_Statements_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymOntoClay.UnityAsset.Core.Tests
+{
+    public class If_else_elif_Statements_Tests
+    {
+    }
+}

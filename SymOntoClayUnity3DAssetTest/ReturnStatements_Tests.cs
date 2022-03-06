6076fd4ccbed985aeb702ab06851350550e85d4b
metatypeman <metatypeman@gmail.com>
2022-03-06 16:42:10 +0200

    snapshot

--- SymOntoClayUnity3DAssetTest/ReturnStatements_Tests.cs
+++ SymOntoClayUnity3DAssetTest/ReturnStatements_Tests.cs
@@ -149,5 +149,54 @@ namespace SymOntoClay.UnityAsset.Core.Tests
                     }
                 }), true);
         }
+
+        [Test]
+        [Parallelizable]
+        public void Case2_a()
+        {
+            var text = @"app PeaceKeeper
+{
+    fun a() => 
+    {
+        '`a` has been called!' >> @>log;
+        @a = 3;
+        return 1 + @a;
+        '`a` has been ended!' >> @>log;
+    }
+
+    on Init =>
+    {
+        'Begin' >> @>log;
+        a() >> @>log;
+        'End' >> @>log;
+    }
+}";
+
+            Assert.AreEqual(BehaviorTestEngineInstance.Run(text,
+                (n, message) =>
+                {
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual(message, "Begin");
+                            break;
+
+                        case 2:
+                            Assert.AreEqual(message, "`a` has been called!");
+                            break;
+
+                        case 3:
+                            Assert.AreEqual(message, "4");
+                            break;
+
+                        case 4:
+                            Assert.AreEqual(message, "End");
+                            break;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }), true);
+        }
     }
 }

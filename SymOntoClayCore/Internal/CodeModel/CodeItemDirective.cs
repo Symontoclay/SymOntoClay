c7f0fdef32695fb93872d14c636e57dbc9949e66
metatypeman <metatypeman@gmail.com>
2022-03-19 18:41:51 +0200

    snapshot

--- SymOntoClayCore/Internal/CodeModel/CodeItemDirective.cs
+++ SymOntoClayCore/Internal/CodeModel/CodeItemDirective.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SymOntoClay.CoreHelper.DebugHelpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,16 +9,57 @@ namespace SymOntoClay.Core.Internal.CodeModel
     {
         public abstract KindOfCodeItemDirective KindOfCodeItemDirective { get; }
 
-        ///// <summary>
-        ///// Clones the instance and returns cloned instance.
-        ///// </summary>
-        ///// <returns>Cloned instance.</returns>
-        //public CodeItemDirective CloneCodeItemDirective()
-        //{
-        //    var context = new Dictionary<object, object>();
-        //    return CloneCodeItemDirective(context);
-        //}
+        /// <summary>
+        /// Clones the instance and returns cloned instance.
+        /// </summary>
+        /// <returns>Cloned instance.</returns>
+        public CodeItemDirective CloneCodeItemDirective()
+        {
+            var context = new Dictionary<object, object>();
+            return CloneCodeItemDirective(context);
+        }
 
+        /// <summary>
+        /// Clones the instance using special context and returns cloned instance.
+        /// </summary>
+        /// <param name="context">Special context for providing references continuity.</param>
+        /// <returns>Cloned instance.</returns>
+        public abstract CodeItemDirective CloneCodeItemDirective(Dictionary<object, object> context);
 
+        /// <inheritdoc/>
+        protected override string PropertiesToString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(KindOfCodeItemDirective)} = {KindOfCodeItemDirective}");
+
+            sb.Append(base.PropertiesToString(n));
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        protected override string PropertiesToShortString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(KindOfCodeItemDirective)} = {KindOfCodeItemDirective}");
+
+            sb.Append(base.PropertiesToShortString(n));
+            return sb.ToString();
+        }
+
+        /// <inheritdoc/>
+        protected override string PropertiesToBriefString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(KindOfCodeItemDirective)} = {KindOfCodeItemDirective}");
+
+            sb.Append(base.PropertiesToBriefString(n));
+            return sb.ToString();
+        }
     }
 }

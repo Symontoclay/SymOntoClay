acd51ec79caf839fef07ce816dad5f00cbe11f60
metatypeman <metatypeman@gmail.com>
2022-04-17 18:05:23 +0300

    NLPConverter initial commit

---
+++ SymOntoClayNLP/INLPConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.NLP
+{
+    //TODO: It should be moved to core or UnityAssetCore
+    public interface INLPConverter
+    {
+    }
+}
---
+++ SymOntoClayNLP/NLPConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.NLP
+{
+    public class NLPConverter: INLPConverter
+    {
+    }
+}

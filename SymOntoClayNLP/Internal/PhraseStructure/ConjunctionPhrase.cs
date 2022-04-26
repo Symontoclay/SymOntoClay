71bbcaa429b067d9e904e499586b2bb93040e535
metatypeman <metatypeman@gmail.com>
2022-04-26 12:32:28 +0300

    ATN snapshot

--- SymOntoClayNLP/Internal/PhraseStructure/ConjunctionPhrase.cs
+++ SymOntoClayNLP/Internal/PhraseStructure/ConjunctionPhrase.cs
@@ -17,11 +17,41 @@ namespace SymOntoClay.NLP.Internal.PhraseStructure
         /// <inheritdoc/>
         public override ConjunctionPhrase AsConjunctionPhrase => this;
 
+        /// <summary>
+        /// The first conjuction in pair of correlative conjunctions.
+        /// "both" in "both cat and dog"
+        /// </summary>
+        public Word PreConj { get; set; }
+
+        /// <summary>
+        /// "cat" in "both cat and dog"
+        /// "cat" in "cat and dog"
+        /// </summary>
+        public BaseSentenceItem Left { get; set; }
+
+        /// <summary>
+        /// The root word of the sentence or second conjuction in pair of correlative conjunctions.
+        /// "and" in "both cat and dog"
+        /// "and" in "cat and dog"
+        /// </summary>
+        public Word Conj { get; set; }
+
+        /// <summary>
+        /// "dog" in "both cat and dog"
+        /// "dog" in "cat and dog"
+        /// </summary>
+        public BaseSentenceItem Right { get; set; }
+
         /// <inheritdoc/>
         protected override string PropertiesToString(uint n)
         {
             var spaces = DisplayHelper.Spaces(n);
             var sb = new StringBuilder();
+
+            sb.PrintObjProp(n, nameof(PreConj), PreConj);
+            sb.PrintObjProp(n, nameof(Left), Left);
+            sb.PrintObjProp(n, nameof(Conj), Conj);
+            sb.PrintObjProp(n, nameof(Right), Right);
 
             //sb.PrintObjProp(n, nameof(), );
 

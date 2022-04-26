43e38ec7ff9e819c63f14fa1cd84888ebdf879c4
metatypeman <metatypeman@gmail.com>
2022-04-26 19:57:41 +0300

    ATN BaseParser

---
+++ SymOntoClayNLP/Internal/ATN/BaseParser.cs
@@ -0,0 +1,35 @@
+﻿using SymOntoClay.NLP.Internal.PhraseStructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.NLP.Internal.ATN
+{
+    public abstract class BaseParser
+    {
+        protected virtual void OnEnter()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void OnRun()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void OnVariant(ConcreteATNToken concreteATNToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void OnReceiveReturn(BaseSentenceItem phrase)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void OnFinish()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SymOntoClayNLP/Internal/ATN/ConcreteATNToken.cs
@@ -0,0 +1,44 @@
+﻿using SymOntoClay.CoreHelper.DebugHelpers;
+using SymOntoClay.NLP.CommonDict;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.NLP.Internal.ATN
+{
+    public class ConcreteATNToken : IObjectToString
+    {
+        public KindOfATNToken Kind { get; set; } = KindOfATNToken.Unknown;
+        public string Content { get; set; } = string.Empty;
+        public int Pos { get; set; }
+        public int Line { get; set; }
+        public BaseGrammaticalWordFrame WordFrame { get; set; }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return ToString(0u);
+        }
+
+        /// <inheritdoc/>
+        public string ToString(uint n)
+        {
+            return this.GetDefaultToStringInformation(n);
+        }
+
+        /// <inheritdoc/>
+        string IObjectToString.PropertiesToString(uint n)
+        {
+            var spaces = DisplayHelper.Spaces(n);
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{spaces}{nameof(Kind)} = {Kind}");
+            sb.AppendLine($"{spaces}{nameof(Content)} = {Content}");
+            sb.AppendLine($"{spaces}{nameof(Pos)} = {Pos}");
+            sb.AppendLine($"{spaces}{nameof(Line)} = {Line}");
+            sb.PrintObjProp(n, nameof(WordFrame), WordFrame);
+
+            return sb.ToString();
+        }
+    }
+}

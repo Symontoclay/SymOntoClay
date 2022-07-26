04874fbff07933abcf6951aba40ec3bf9e4b833b
metatypeman <metatypeman@gmail.com>
2022-07-26 19:49:40 +0300

    snapshot

--- SymOntoClayUnityAssetCore/IStandardFactsBuilder.cs
+++ SymOntoClayUnityAssetCore/IStandardFactsBuilder.cs
@@ -8,6 +8,7 @@ namespace SymOntoClay.UnityAsset.Core
     public interface IStandardFactsBuilder
     {
         string BuildSayFactString(string selfId, string factStr);
+        RuleInstance BuildSayFactInstance(string selfId, RuleInstance fact);
         string BuildSoundFactString(double power, double distance, float directionToPosition, string factStr);
         RuleInstance BuildSoundFactInstance(double power, double distance, float directionToPosition, RuleInstance fact);
     }

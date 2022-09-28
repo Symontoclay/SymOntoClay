a6dbc14ba289c7dff3aeffc8b978851d4095ccff
metatypeman <metatypeman@gmail.com>
2022-09-28 20:14:24 +0300

    0.4.2 snapshot

--- SymOntoClayUnityAssetCore/IExecutorInMainThread.cs
+++ SymOntoClayUnityAssetCore/IExecutorInMainThread.cs
@@ -6,7 +6,18 @@ namespace SymOntoClay.UnityAsset.Core
 {
     public interface IExecutorInMainThread
     {
+        /// <summary>
+        /// Executes handler in main thread context.
+        /// </summary>
+        /// <param name="function">Handler which should be executed in main thread context.</param>
         void RunInMainThread(Action function);
+
+        /// <summary>
+        /// Executes handler in main thread context.
+        /// </summary>
+        /// <typeparam name="TResult">Type of result.</typeparam>
+        /// <param name="function">Handler which should be executed in main thread context.</param>
+        /// <returns>Result of the execution.</returns>
         TResult RunInMainThread<TResult>(Func<TResult> function);
     }
 }

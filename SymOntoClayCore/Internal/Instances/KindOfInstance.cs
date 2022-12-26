dedee68b07d2dd74a7ff4f3c92f8c0b472a8aedf
metatypeman <metatypeman@gmail.com>
2022-12-26 13:54:11 +0200

    snapshot

--- SymOntoClayCore/Internal/Instances/KindOfInstance.cs
+++ SymOntoClayCore/Internal/Instances/KindOfInstance.cs
@@ -31,6 +31,7 @@ namespace SymOntoClay.Core.Internal.Instances
         Unknown,
         AppInstance,
         StateInstance,
-        ActionInstance
+        ActionInstance,
+        ObjectInstance
     }
 }
---
+++ SymOntoClayCore/Internal/Instances/ObjectInstance.cs
@@ -0,0 +1,20 @@
+﻿using SymOntoClay.Core.Internal.CodeExecution;
+using SymOntoClay.Core.Internal.CodeModel;
+using SymOntoClay.Core.Internal.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.Core.Internal.Instances
+{
+    public class ObjectInstance: BaseIndependentInstance
+    {
+        public ObjectInstance(CodeItem codeItem, IEngineContext context, IStorage parentStorage, LocalCodeExecutionContext parentCodeExecutionContext)
+            : base(codeItem, context, parentStorage, parentCodeExecutionContext, new ObjectStorageFactory(), null)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override KindOfInstance KindOfInstance => KindOfInstance.ObjectInstance;
+    }
+}

7b72e48eef224b78dca747e7bb985acce1f95f48
metatypeman <metatypeman@gmail.com>
2023-08-29 13:59:02 +0300

    0.5.3 SymOntoClay.Monitor.LogFileBuilder

---
+++ SymOntoClay.Monitor.LogFileBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SymOntoClay.Monitor.LogFileBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

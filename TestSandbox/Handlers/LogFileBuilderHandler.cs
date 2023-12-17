a78a6d3724cde7f144c4754be332d4c9e1b9a0c8
metatypeman <metatypeman@gmail.com>
2023-12-17 14:52:04 +0200

    0.5.3 snapshot

--- TestSandbox/Handlers/LogFileBuilderHandler.cs
+++ TestSandbox/Handlers/LogFileBuilderHandler.cs
@@ -46,7 +46,7 @@ namespace TestSandbox.Handlers
 
         private void Case13()
         {
-            RunLogFileBuilderProgramMain(@"--i c:\Users\Acer\AppData\Roaming\SymOntoClayAsset\NpcLogMessages\2023_12_17_14_24_28\ --o c:\Users\Acer\source\repos\SymOntoClay\TestSandbox\bin\Debug\net7.0\MessagesLogsOutputDir\ --target-nodeid #BlueSoldier");
+            RunLogFileBuilderProgramMain(@"--i c:\Users\Acer\AppData\Roaming\SymOntoClayAsset\NpcLogMessages\2023_12_17_14_39_16\ --o c:\Users\Acer\source\repos\SymOntoClay\TestSandbox\bin\Debug\net7.0\MessagesLogsOutputDir\ --target-nodeid #BlueSoldier");
         }
 
         private void Case12()

1872bdaa38cef8f442381691fd9fb6ba2f75ba56
metatypeman <metatypeman@gmail.com>
2024-05-02 21:13:31 +0300

    0.5.4 replacing to nupkg

--- SymOntoClayCoreHelper/ISymOntoClayDisposable.cs
+++ SymOntoClayCoreHelper/ISymOntoClayDisposable.cs
@@ -24,16 +24,17 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SymOntoClay.CoreHelper
-{
-    /// <summary>
-    /// Provides small improving of mechanism for releasing unmanaged resources by adding additional members.
-    /// </summary>
-    public interface ISymOntoClayDisposable : IDisposable
-    {
-        /// <summary>
-        /// Returns <c>true</c> if the instance was disposed, otherwise returns <c>false</c>.
-        /// </summary>
-        bool IsDisposed { get; }
-    }
-}
+//namespace SymOntoClay.CoreHelper
+//{
+//    /// <summary>
+//    /// Provides small improving of mechanism for releasing unmanaged resources by adding additional members.
+//    /// </summary>
+//    [Obsolete("Use SymOntoClay.Common.Disposing.ISymOntoClayDisposable")]
+//    public interface ISymOntoClayDisposable : IDisposable
+//    {
+//        /// <summary>
+//        /// Returns <c>true</c> if the instance was disposed, otherwise returns <c>false</c>.
+//        /// </summary>
+//        bool IsDisposed { get; }
+//    }
+//}

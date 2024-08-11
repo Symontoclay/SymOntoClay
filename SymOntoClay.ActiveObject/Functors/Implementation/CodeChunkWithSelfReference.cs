67428c821e400330fb91ed9521668d0cc4449d70
metatypeman <metatypeman@gmail.com>
2024-08-11 18:48:36 +0300

    0.5.5 serialization

--- SymOntoClay.ActiveObject/Functors/Implementation/CodeChunkWithSelfReference.cs
+++ SymOntoClay.ActiveObject/Functors/Implementation/CodeChunkWithSelfReference.cs
@@ -22,22 +22,51 @@ namespace SymOntoClay.ActiveObject.Functors.Implementation
 
         private bool _isFinished;
         private bool _actionIsFinished;
-        private List<IBaseCodeChunk> _chunks = new List<IBaseCodeChunk>();
+        private List<IBaseCodeChunk> _children = new List<IBaseCodeChunk>();
 
         /// <inheritdoc/>
         public void CreateCodeChunk(string chunkId, Action action)
         {
             var chunk = new CodeChunk(chunkId, _codeChunksContext, action);
-            _chunks.Add(chunk);
+            _children.Add(chunk);
         }
 
         /// <inheritdoc/>
         public void CreateCodeChunk(string chunkId, Action<ICodeChunkWithSelfReference> action)
         {
             var chunk = new CodeChunkWithSelfReference(chunkId, _codeChunksContext, action);
-            _chunks.Add(chunk);
+            _children.Add(chunk);
         }
 
+        /// <inheritdoc/>
+        public void Run()
+        {
+            if (_isFinished)
+            {
+                return;
+            }
 
+            if (!_actionIsFinished)
+            {
+                _action(this);
+
+                _actionIsFinished = true;
+            }
+
+            foreach (var child in _children)
+            {
+                if (child.IsFinished)
+                {
+                    continue;
+                }
+
+                child.Run();
+            }
+
+            _isFinished = true;
+        }
+
+        /// <inheritdoc/>
+        public bool IsFinished => _isFinished;
     }
 }

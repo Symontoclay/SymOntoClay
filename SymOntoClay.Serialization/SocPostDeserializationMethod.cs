586c4bb58520d1de00bd2cb8b20259a00a943063
metatypeman <metatypeman@gmail.com>
2024-08-24 12:38:24 +0300

    0.5.5 serialization: added SocPostDeserializationMethod

---
+++ SymOntoClay.Serialization/SocPostDeserializationMethod.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SymOntoClay.Serialization
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public class SocPostDeserializationMethod : Attribute
+    {
+    }
+}

8830f0464d1de58536363104502c19d60a46b5a0
metatypeman <metatypeman@gmail.com>
2024-09-10 08:26:38 +0300

    0.5.5 serialization: refactoring

---
+++ SymOntoClay.ActiveObject/Functors/BaseSyncFunctorWithResult.cs
@@ -0,0 +1,35 @@
+﻿using SymOntoClay.Monitor.Common;
+using SymOntoClay.Serialization;
+using System;
+using static System.Collections.Specialized.BitVector32;
+
+namespace SymOntoClay.ActiveObject.Functors
+{
+    public partial class BaseSyncFunctorWithResult<TResult> : IBaseFunctor
+    {
+        public BaseSyncFunctorWithResult(IMonitorLogger logger, string functorId, Func<TResult> func, ISerializationAnchor serializationAnchor)
+        {
+            _serializationAnchor = serializationAnchor;
+            serializationAnchor.AddFunctor(this);
+            _functorId = functorId;
+
+            _func = func;
+            _logger = logger;
+        }
+
+        [SocSerializableActionKey]
+        private string _functorId;
+
+        private ISerializationAnchor _serializationAnchor;
+
+        private readonly Func<TResult> _func;
+
+        private IMonitorLogger _logger;
+
+        private bool _isFinished;
+
+        private TResult _result;
+
+        public void Run();
+    }
+}
---
+++ SymOntoClay.ActiveObject/Functors/BaseSyncFunctorWithoutResult.cs
@@ -0,0 +1,33 @@
+﻿using SymOntoClay.Monitor.Common;
+using SymOntoClay.Serialization;
+using System;
+
+namespace SymOntoClay.ActiveObject.Functors
+{
+    public partial class BaseSyncFunctorWithoutResult : IBaseFunctor
+    {
+        public BaseSyncFunctorWithoutResult(IMonitorLogger logger, string functorId, Action action, ISerializationAnchor serializationAnchor)
+        {
+            _serializationAnchor = serializationAnchor;
+            serializationAnchor.AddFunctor(this);
+
+            _functorId = functorId;
+
+            _action = action;
+            _logger = logger;
+        }
+
+        [SocSerializableActionKey]
+        private string _functorId;
+
+        private ISerializationAnchor _serializationAnchor;
+
+        private readonly Action _action;
+
+        private IMonitorLogger _logger;
+
+        private bool _isFinished;
+
+        public void Run();
+    }
+}

e4acad2e2b258c3f17ea1f1b1175f6a11e1cb587
metatypeman <metatypeman@gmail.com>
2024-09-12 20:09:00 +0300

    0.5.5 serialization: refactoring

---
+++ SymOntoClay.ActiveObject/Threads/IActiveAsyncObject.cs
@@ -0,0 +1,13 @@
+﻿using SymOntoClay.Threading;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.ActiveObject.Threads
+{
+    public interface IActiveAsyncObject: IActiveObject
+    {
+        IThreadTask TaskValue { get; }
+        IThreadTask Start();
+    }
+}

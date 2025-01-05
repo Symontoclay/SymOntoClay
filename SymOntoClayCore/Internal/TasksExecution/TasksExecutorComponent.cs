0c095858162d5a6ada5485a849e2c515e437722c
metatypeman <metatypeman@gmail.com>
2025-01-05 17:59:31 +0200

    0.5.5 htn recursion: initial commit

--- SymOntoClayCore/Internal/TasksExecution/TasksExecutorComponent.cs
+++ SymOntoClayCore/Internal/TasksExecution/TasksExecutorComponent.cs
@@ -39,48 +39,50 @@ namespace SymOntoClay.Core.Internal.TasksExecution
             Info("EB501AF1-9B30-4D5A-ACD1-C013DF7769B8", $"_executionState = {_executionState}");
 #endif
 
-            if(_executionState == ExecutionState.Init)
-            {
-                _tasksPlanner = new TasksPlanner(_context);
+//            if(_executionState == ExecutionState.Init)
+//            {
+//                _tasksPlanner = new TasksPlanner(_context);
 
-                if (!_tasksPlanner.HasRootTasks)
-                {
-#if DEBUG
-                    Info("723CD4E5-CCF4-4C1E-AF2D-DCDA776CA721", "!_tasksPlanner.HasRootTasks");
-#endif
+//                if (!_tasksPlanner.HasRootTasks)
+//                {
+//#if DEBUG
+//                    Info("723CD4E5-CCF4-4C1E-AF2D-DCDA776CA721", "!_tasksPlanner.HasRootTasks");
+//#endif
 
-                    return false;
-                }
+//                    return false;
+//                }
 
-                _executionState = ExecutionState.WaitingForPlanBuilding;
-            }
+//                _executionState = ExecutionState.WaitingForPlanBuilding;
+//            }
 
-            if(_executionState == ExecutionState.WaitingForPlanBuilding)
-            {
-                Logger.StartBuildPlan("584A66AE-6592-4476-AA54-C283C9A65DBE");
+//            if(_executionState == ExecutionState.WaitingForPlanBuilding)
+//            {
+//                Logger.StartBuildPlan("584A66AE-6592-4476-AA54-C283C9A65DBE");
 
-                _plan = _tasksPlanner.BuildPlan();
+//                _plan = _tasksPlanner.BuildPlan();
 
-                Logger.StopBuildPlan("CEB8C398-CFA8-4869-A6BC-5BFB32F89CBF");
+//                Logger.StopBuildPlan("CEB8C398-CFA8-4869-A6BC-5BFB32F89CBF");
 
-                _executionState = ExecutionState.WaitingForRunnerCreation;
-            }
+//                _executionState = ExecutionState.WaitingForRunnerCreation;
+//            }
 
-            if(_executionState == ExecutionState.WaitingForRunnerCreation)
-            {
-                _tasksPlanRunner = new TasksPlanRunner(_context, new AsyncActivePeriodicObject(_context.ActiveObjectContext, _context.CodeExecutionThreadPool, _context.Logger));
+//            if(_executionState == ExecutionState.WaitingForRunnerCreation)
+//            {
+//                _tasksPlanRunner = new TasksPlanRunner(_context, new AsyncActivePeriodicObject(_context.ActiveObjectContext, _context.CodeExecutionThreadPool, _context.Logger));
 
-                _executionState = ExecutionState.WaitingForPlanExecution;
-            }
+//                _executionState = ExecutionState.WaitingForPlanExecution;
+//            }
 
-            if(_executionState == ExecutionState.WaitingForPlanExecution)
-            {
-                _tasksPlanRunner.Run(_plan);
+//            if(_executionState == ExecutionState.WaitingForPlanExecution)
+//            {
+//                _tasksPlanRunner.Run(_plan);
                 
-                _executionState = ExecutionState.WaitingForPlanBuilding;
-            }
+//                _executionState = ExecutionState.WaitingForPlanBuilding;
+//            }
 
-            return true;
+//            return true;
+
+            return false;//tmp
         }
 
         /// <inheritdoc/>

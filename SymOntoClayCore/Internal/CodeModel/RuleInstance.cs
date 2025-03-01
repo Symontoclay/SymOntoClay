9cb2fbface128490fc92156cff6da61cb3776843
metatypeman <metatypeman@gmail.com>
2025-03-01 15:11:41 +0200

    0.5.5 removed FrameId

--- SymOntoClayCore/Internal/CodeModel/RuleInstance.cs
+++ SymOntoClayCore/Internal/CodeModel/RuleInstance.cs
@@ -66,8 +66,6 @@ namespace SymOntoClay.Core.Internal.CodeModel
         [ResolveToType(typeof(LogicalValue))]
         public Value SelfObligationModality { get; set; }
 
-        public ulong FrameId { get; set; }
-
         public List<StrongIdentifierValue> UsedKeysList { get; set; }
 
         public List<LogicalQueryNode> LeavesList { get; set; }
@@ -340,8 +338,6 @@ namespace SymOntoClay.Core.Internal.CodeModel
             sb.PrintObjProp(n, nameof(ObligationModality), ObligationModality);
             sb.PrintObjProp(n, nameof(SelfObligationModality), SelfObligationModality); 
 
-            sb.AppendLine($"{spaces}{nameof(FrameId)} = {FrameId}");
-
             sb.PrintObjListProp(n, nameof(UsedKeysList), UsedKeysList);
             sb.PrintObjListProp(n, nameof(LeavesList), LeavesList);
 
@@ -369,8 +365,6 @@ namespace SymOntoClay.Core.Internal.CodeModel
             sb.PrintShortObjProp(n, nameof(ObligationModality), ObligationModality);
             sb.PrintShortObjProp(n, nameof(SelfObligationModality), SelfObligationModality);
 
-            sb.AppendLine($"{spaces}{nameof(FrameId)} = {FrameId}");
-
             sb.PrintShortObjListProp(n, nameof(UsedKeysList), UsedKeysList);
             sb.PrintShortObjListProp(n, nameof(LeavesList), LeavesList);
 
@@ -397,8 +391,6 @@ namespace SymOntoClay.Core.Internal.CodeModel
 
             sb.PrintExisting(n, nameof(ObligationModality), ObligationModality);
             sb.PrintExisting(n, nameof(SelfObligationModality), SelfObligationModality);
-
-            sb.AppendLine($"{spaces}{nameof(FrameId)} = {FrameId}");
 
             sb.PrintBriefObjListProp(n, nameof(UsedKeysList), UsedKeysList);
             sb.PrintBriefObjListProp(n, nameof(LeavesList), LeavesList);

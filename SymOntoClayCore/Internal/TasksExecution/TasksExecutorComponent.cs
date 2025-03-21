6d1e736f0d2fbfa4c94f0655e277ddecdae04e63
metatypeman <metatypeman@gmail.com>
2025-03-21 20:49:28 +0200

    0.5.5 htn

--- SymOntoClayCore/Internal/TasksExecution/TasksExecutorComponent.cs
+++ SymOntoClayCore/Internal/TasksExecution/TasksExecutorComponent.cs
@@ -46,7 +46,7 @@ namespace SymOntoClay.Core.Internal.TasksExecution
         private volatile TasksPlan _plan;
         private volatile IThreadExecutor _threadExecutor;
 
-        private int? _maxPlanExecutionIterationsCount = 0;
+        private int? _maxPlanExecutionIterationsCount = 1;
         private volatile int _runPlanExecutionIterations;
 
         public void BeginStarting()
@@ -88,7 +88,7 @@ namespace SymOntoClay.Core.Internal.TasksExecution
                 if (!_tasksPlanner.HasRootTasks)
                 {
 #if DEBUG
-                    //Info("723CD4E5-CCF4-4C1E-AF2D-DCDA776CA721", "!_tasksPlanner.HasRootTasks");
+                    Info("723CD4E5-CCF4-4C1E-AF2D-DCDA776CA721", "!_tasksPlanner.HasRootTasks");
 #endif
 
                     return false;
@@ -105,12 +105,21 @@ namespace SymOntoClay.Core.Internal.TasksExecution
 
                 Logger.StopBuildPlan("CEB8C398-CFA8-4869-A6BC-5BFB32F89CBF");
 
+#if DEBUG
+                //Info("3271453A-496B-4BE6-8D14-EDF93444E44B", $"_plan = {_plan}");
+                Info("204B2435-FC12-46EB-B869-CADAEB74FD9A", $"_plan = {_plan.ToDbgString()}");
+#endif
+
                 _executionState = ExecutionState.WaitingForStartPlanExecution;
             }
 
             if (_executionState == ExecutionState.WaitingForStartPlanExecution)
             {
                 var compiledFunctionBody = _compiler.Compile(_plan);
+
+#if DEBUG
+                Info("4B419A0B-0396-4714-AAA9-4E94F4251178", $"compiledFunctionBody = {compiledFunctionBody.ToDbgString()}");
+#endif
 
                 var mainEntity = _context.InstancesStorage.MainEntity;
 

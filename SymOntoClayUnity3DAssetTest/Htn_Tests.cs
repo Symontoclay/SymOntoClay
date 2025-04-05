a11508bd632f0cf7ac0faebaa3ee52d12d90af9f
metatypeman <metatypeman@gmail.com>
2025-04-05 18:59:52 +0300

    0.5.5 htn: added test with minimal autoproperty

--- SymOntoClayUnity3DAssetTest/Htn_Tests.cs
+++ SymOntoClayUnity3DAssetTest/Htn_Tests.cs
@@ -626,5 +626,118 @@ primitive task SomePrimitiveTask4
 
             Assert.AreEqual(12, maxN);
         }
+
+        [Test]
+        [Parallelizable]
+        public void AutoPropertyCase1()
+        {
+            var text = @"app PeaceKeeper
+{
+    root task `DestroyEnemy`;
+
+    fun ChooseEnemyOperator()
+    {
+       'Run ChooseEnemyOperator' >> @>log;
+       wait 1;
+    }
+
+    fun NavToEnemyOperator(@target)
+    {
+       'Run NavToEnemyOperator' >> @>log;
+       @target >> @>log;
+       wait 1;
+    }
+
+    fun KillEnemyOperator(@target)
+    {
+       'Run KillEnemyOperator' >> @>log;
+       @target >> @>log;
+       wait 1;
+    }
+
+    prop TargetEnemy: number = 22;
+}
+
+compound task DestroyEnemy
+{
+   case
+   {
+       ChooseEnemy;
+       NavToEnemy;
+       KillEnemy;
+   }
+}
+
+primitive task ChooseEnemy
+{
+    operator ChooseEnemyOperator();
+}
+
+primitive task NavToEnemy
+{
+    operator NavToEnemyOperator(TargetEnemy);
+}
+
+primitive task KillEnemy
+{
+    operator KillEnemyOperator(TargetEnemy);
+}";
+
+            var maxN = 0;
+
+            Assert.AreEqual(true, BehaviorTestEngineRunner.RunMinimalInstance(text,
+                (n, message) =>
+                {
+                    maxN = n;
+
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual("Run ChooseEnemyOperator", message);
+                            return true;
+
+                        case 2:
+                            Assert.AreEqual("Run NavToEnemyOperator", message);
+                            return true;
+
+                        case 3:
+                            Assert.AreEqual("22", message);
+                            return true;
+
+                        case 4:
+                            Assert.AreEqual("Run KillEnemyOperator", message);
+                            return true;
+
+                        case 5:
+                            Assert.AreEqual("22", message);
+                            return true;
+
+                        case 6:
+                            Assert.AreEqual("Run ChooseEnemyOperator", message);
+                            return true;
+
+                        case 7:
+                            Assert.AreEqual("Run NavToEnemyOperator", message);
+                            return true;
+
+                        case 8:
+                            Assert.AreEqual("22", message);
+                            return true;
+
+                        case 9:
+                            Assert.AreEqual("Run KillEnemyOperator", message);
+                            return true;
+
+                        case 10:
+                            Assert.AreEqual("22", message);
+                            return false;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }));
+
+            Assert.AreEqual(10, maxN);
+        }
     }
 }

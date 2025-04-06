6a8dad38da4493166ffc74815aae26d1b4ce43b9
metatypeman <metatypeman@gmail.com>
2025-04-06 20:00:55 +0300

    0.5.5 refactoring

--- SymOntoClayCore/Internal/DataResolvers/MethodsResolver.cs
+++ SymOntoClayCore/Internal/DataResolvers/MethodsResolver.cs
@@ -29,7 +29,6 @@ using SymOntoClay.Monitor.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 namespace SymOntoClay.Core.Internal.DataResolvers
 {

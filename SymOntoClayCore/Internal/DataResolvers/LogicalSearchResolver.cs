2cc1022d97dbbb2030dcb8bd1c0b8e1bd5c345ff
metatypeman <metatypeman@gmail.com>
2025-04-21 19:20:32 +0300

    0.5.5 refactoring

--- SymOntoClayCore/Internal/DataResolvers/LogicalSearchResolver.cs
+++ SymOntoClayCore/Internal/DataResolvers/LogicalSearchResolver.cs
@@ -20,7 +20,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
-using Newtonsoft.Json;
 using SymOntoClay.Common.CollectionsHelpers;
 using SymOntoClay.Core.DebugHelpers;
 using SymOntoClay.Core.Internal.CodeModel;

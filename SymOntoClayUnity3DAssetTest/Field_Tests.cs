c56f2234deeb547c0c9f0c360ad6a619f7e67602
metatypeman <metatypeman@gmail.com>
2025-04-23 19:35:47 +0300

    0.5.5 type conversion in field init: added test

--- SymOntoClayUnity3DAssetTest/Field_Tests.cs
+++ SymOntoClayUnity3DAssetTest/Field_Tests.cs
@@ -741,5 +741,49 @@ namespace SymOntoClay.UnityAsset.Core.Tests
 
             Assert.AreEqual(3, maxN);
         }
+
+        [Test]
+        [Parallelizable]
+        public void TypeConversion_Case1()
+        {
+            var text = @"app PeaceKeeper
+{
+    @b: boolean = {: enemy($_) & see(I, $_) & alive($_, true) :};
+
+    on Enter =>
+    {
+        'Begin' >> @>log;
+        @b >> @>log;
+        'End' >> @>log;
+    }
+}";
+
+            var maxN = 0;
+
+            Assert.AreEqual(BehaviorTestEngineRunner.RunMinimalInstance(text,
+                (n, message) => {
+                    maxN = n;
+
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual(message, "Begin");
+                            return true;
+
+                        case 2:
+                            Assert.AreEqual(message, "0");
+                            return true;
+
+                        case 3:
+                            Assert.AreEqual(message, "End");
+                            return false;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }), true);
+
+            Assert.AreEqual(3, maxN);
+        }
     }
 }

1f12f552f8e2736afc25ed018016932f2297bb79
metatypeman <metatypeman@gmail.com>
2025-05-05 20:33:26 +0300

    0.5.5 names refactoring

--- SymOntoClayCore/Internal/Parsing/Internal/StrongIdentifierValueParser.cs
+++ SymOntoClayCore/Internal/Parsing/Internal/StrongIdentifierValueParser.cs
@@ -24,6 +24,7 @@ namespace SymOntoClay.Core.Internal.Parsing.Internal
         private List<StrongIdentifierPart> _items;
         private List<StrongIdentifierPart> _currentItemsList;
         private StrongIdentifierPart _currentItem;
+        private Stack<List<StrongIdentifierPart>> _itemsListStack = new Stack<List<StrongIdentifierPart>>();
 
         /// <inheritdoc/>
         protected override void OnEnter()
@@ -42,17 +43,22 @@ namespace SymOntoClay.Core.Internal.Parsing.Internal
 #if DEBUG
             Info("1B5ACEB2-D3E6-4E0A-8341-53BF158773C4", $"_items = {_items.WriteListToString()}");
 #endif
+
+            if(_currentItemsList != _items)
+            {
+                throw new NotImplementedException();
+            }
         }
 
         /// <inheritdoc/>
         protected override void OnRun()
         {
 #if DEBUG
-            Info("E11076A3-E467-41F3-A228-B95D0F17FB5F", $"_state = {_state}");
-            Info("60F3CC3D-9E06-4F5C-8E0C-1C92DFF8A226", $"_currToken = {_currToken}");
-            Info("120DA08F-7326-4AB0-A1FB-EBF9DDB85631", $"_items = {_items.WriteListToString()}");
-            Info("55696EE0-358F-4F13-ABE9-47278918A598", $"_currentItemsList = {_currentItemsList.WriteListToString()}");
-            Info("0E8EB277-F964-4E68-B02A-DD6C12AE195B", $"_currentItem = {_currentItem}");
+            //Info("E11076A3-E467-41F3-A228-B95D0F17FB5F", $"_state = {_state}");
+            //Info("60F3CC3D-9E06-4F5C-8E0C-1C92DFF8A226", $"_currToken = {_currToken}");
+            //Info("120DA08F-7326-4AB0-A1FB-EBF9DDB85631", $"_items = {_items.WriteListToString()}");
+            //Info("55696EE0-358F-4F13-ABE9-47278918A598", $"_currentItemsList = {_currentItemsList.WriteListToString()}");
+            //Info("0E8EB277-F964-4E68-B02A-DD6C12AE195B", $"_currentItem = {_currentItem}");
 #endif
 
             switch (_state)
@@ -95,6 +101,14 @@ namespace SymOntoClay.Core.Internal.Parsing.Internal
                             }
                             break;
 
+                        case TokenKind.OpenRoundBracket:
+                            StartParsingSubParts();
+                            break;
+
+                        case TokenKind.CloseRoundBracket:
+                            StopParsingSubParts();
+                            break;
+
                         default:
                             throw new UnexpectedTokenException(_currToken);
                     }
@@ -108,7 +122,7 @@ namespace SymOntoClay.Core.Internal.Parsing.Internal
         private StrongIdentifierLevel ParseStrongIdentifierLevel(KeyWordTokenKind keyWordTokenKind)
         {
 #if DEBUG
-            Info("34250D69-480C-42E6-B335-9578BF8E402D", $"keyWordTokenKind = {keyWordTokenKind}");
+            //Info("34250D69-480C-42E6-B335-9578BF8E402D", $"keyWordTokenKind = {keyWordTokenKind}");
 #endif
 
             switch (keyWordTokenKind)
@@ -133,11 +147,22 @@ namespace SymOntoClay.Core.Internal.Parsing.Internal
         private void AddItem(StrongIdentifierPart item)
         {
 #if DEBUG
-            Info("3463403E-245A-4E33-95BA-D0EBEF267B41", $"item = {item}");
+            //Info("3463403E-245A-4E33-95BA-D0EBEF267B41", $"item = {item}");
 #endif
 
             _currentItem = item;
             _currentItemsList.Add(item);
+        }
+
+        private void StartParsingSubParts()
+        {
+            _itemsListStack.Push(_currentItemsList);
+            _currentItemsList = _currentItem.SubParts;
+        }
+
+        private void StopParsingSubParts()
+        {
+            _currentItemsList = _itemsListStack.Pop();
         }
     }
 }

70826086b077c80b373f144576b695b2e09f4c42
metatypeman <metatypeman@gmail.com>
2025-07-15 18:26:49 +0300

    0.5.5 HTN with background DSL

--- TestSandbox/Handlers/GeneralStartHandler.cs
+++ TestSandbox/Handlers/GeneralStartHandler.cs
@@ -102,7 +102,7 @@ namespace TestSandbox.Handlers
 
             _globalLogger.Info($"sourceDirectoryName = {sourceDirectoryName}");
 
-            Thread.Sleep(1000);
+            Thread.Sleep(10000);
 
             _logger.Info("E90B79FF-F642-4FC1-94A6-EE3F9BBD6DD0", "|||||||||||||");
 

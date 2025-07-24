5484c0e677df78275ad10772c9ce99b383443055
metatypeman <metatypeman@gmail.com>
2025-07-24 20:13:05 +0300

    0.5.5 htn background planner

--- SymOntoClayCore/Internal/Instances/TaskInstances/BaseCompoundTaskInstance.cs
+++ SymOntoClayCore/Internal/Instances/TaskInstances/BaseCompoundTaskInstance.cs
@@ -23,7 +23,7 @@ namespace SymOntoClay.Core.Internal.Instances.TaskInstances
         {
             base.CreateConditionalTriggers(logger);
 
-
+            //CompoundHtnTaskBackgroundTriggerInstance
         }
     }
 }

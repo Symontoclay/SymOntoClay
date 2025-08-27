51e54da6e34ce145de668457965199bfd53f61df
metatypeman <metatypeman@gmail.com>
2025-08-27 20:42:37 +0300

    0.5.5 log file builder: stat

--- SymOntoClay.Monitor.LogFileBuilder/StatLogFileCreator.cs
+++ SymOntoClay.Monitor.LogFileBuilder/StatLogFileCreator.cs
@@ -1,4 +1,6 @@
 ﻿using NLog;
+using System;
+using System.IO;
 using System.Linq;
 
 namespace SymOntoClay.Monitor.LogFileBuilder
@@ -12,7 +14,7 @@ namespace SymOntoClay.Monitor.LogFileBuilder
         public static void Run(LogFileCreatorOptions options, ILogger logger)
         {
 #if DEBUG
-            _logger.Info($"options = {options}");
+            //_logger.Info($"options = {options}");
 #endif
 
             options = options.Clone();
@@ -20,28 +22,48 @@ namespace SymOntoClay.Monitor.LogFileBuilder
             LogFileCreatorOptionsHelper.PrepareOptions(options, logger);
 
 #if DEBUG
-            _logger.Info($"options (after) = {options}");
+            //_logger.Info($"options (after) = {options}");
 #endif
+
+            var now = DateTime.Now;
+
+            var outputFileName = Path.Combine(options.OutputDirectory, $"Stat_{now.ToString(LogFileBuilderConstants.LongDateTimeFormat).Replace(':', '_').Trim()}.txt");
+
+            var outputSw = new StreamWriter(outputFileName);
+            outputSw.AutoFlush = true;
+            outputSw.WriteLine("Statistic:");
+            outputSw.WriteLine(now.ToString(LogFileBuilderConstants.LongDateTimeFormat));
+            outputSw.WriteLine($"Source: '{options.SourceDirectoryName}'");            
+            outputSw.WriteLine();
 
             var fileNamesList = MessageFilesReader.GetFileNames(options.SourceDirectoryName, null, null, null);
 
 #if DEBUG
-            _logger.Info($"fileNamesList.Count = {fileNamesList.Count}");
+            //_logger.Info($"fileNamesList.Count = {fileNamesList.Count}");
 #endif
 
             var fileNamesDictByNodes = fileNamesList.GroupBy(p => p.Item1.NodeId).ToDictionary(p => p.Key, p => p.ToList());
 
 #if DEBUG
-            _logger.Info($"fileNamesDictByNodes.Count = {fileNamesDictByNodes.Count}");
+            //_logger.Info($"fileNamesDictByNodes.Count = {fileNamesDictByNodes.Count}");
 #endif
 
-            foreach(var fileNamesByNodesKvpItem in fileNamesDictByNodes)
+            outputSw.WriteLine($"Messages: {fileNamesList.Count}");
+            outputSw.WriteLine($"Nodes: {fileNamesDictByNodes.Count}");
+            outputSw.WriteLine();
+
+            foreach (var fileNamesByNodesKvpItem in fileNamesDictByNodes.OrderByDescending(p => p.Value.Count))
             {
 #if DEBUG
-                _logger.Info($"fileNamesByNodesKvpItem.Key = {fileNamesByNodesKvpItem.Key}");
-                _logger.Info($"fileNamesByNodesKvpItem.Value.Count = {fileNamesByNodesKvpItem.Value.Count}");
-                _logger.Info($"fileNamesByNodesKvpItem.Value.Count(p => p.Item1.KindOfMessage == Common.Data.KindOfMessage.Error || p.Item1.KindOfMessage == Common.Data.KindOfMessage.Fatal) = {fileNamesByNodesKvpItem.Value.Count(p => p.Item1.KindOfMessage == Common.Data.KindOfMessage.Error || p.Item1.KindOfMessage == Common.Data.KindOfMessage.Fatal)}");
+                //_logger.Info($"fileNamesByNodesKvpItem.Key = {fileNamesByNodesKvpItem.Key}");
+                //_logger.Info($"fileNamesByNodesKvpItem.Value.Count = {fileNamesByNodesKvpItem.Value.Count}");
+                //_logger.Info($"fileNamesByNodesKvpItem.Value.Count(p => p.Item1.KindOfMessage == Common.Data.KindOfMessage.Error || p.Item1.KindOfMessage == Common.Data.KindOfMessage.Fatal) = {fileNamesByNodesKvpItem.Value.Count(p => p.Item1.KindOfMessage == Common.Data.KindOfMessage.Error || p.Item1.KindOfMessage == Common.Data.KindOfMessage.Fatal)}");
 #endif
+
+                outputSw.WriteLine($"Node Id: {fileNamesByNodesKvpItem.Key}");
+                outputSw.WriteLine($"Messages: {fileNamesByNodesKvpItem.Value.Count}");
+                outputSw.WriteLine($"Errors: {fileNamesByNodesKvpItem.Value.Count(p => p.Item1.KindOfMessage == Common.Data.KindOfMessage.Error || p.Item1.KindOfMessage == Common.Data.KindOfMessage.Fatal)}");
+                outputSw.WriteLine();
             }
         }
     }

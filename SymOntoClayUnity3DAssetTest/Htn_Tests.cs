c0e10acd507a7c460dedd362b4b9197ff4c81f89
metatypeman <metatypeman@gmail.com>
2025-10-05 14:28:21 +0300

    0.5.5 THN: checking conditions: added precondition in compound task test

--- SymOntoClayUnity3DAssetTest/Htn_Tests.cs
+++ SymOntoClayUnity3DAssetTest/Htn_Tests.cs
@@ -997,6 +997,96 @@ primitive task KillEnemy
 
         [Test]
         [Parallelizable]
+        public void CompoundHtnTaskCasePreCondition_Case1()
+        {
+            var text = @"app PeaceKeeper
+{
+    root task `SomeCompoundTask`;
+
+    fun SomeOperator()
+    {
+       'Run SomeOperator' >> @>log;
+       wait 1;
+    }
+
+    fun SomeOperator2()
+    {
+       'Run SomeOperator2' >> @>log;
+       wait 1;
+    }
+
+    fun SomeOperator3()
+    {
+       'Run SomeOperator3' >> @>log;
+       wait 1;
+    }
+
+    prop SomeAutoProp: number = 16;
+    prop OtherAutoProp: number = 42;
+}
+
+compound task SomeCompoundTask
+{
+   preconditions OtherAutoProp is 42;  
+
+   case SomeAutoProp is 16
+   {
+       SomePrimitiveTask;
+   }
+
+   case SomeAutoProp > 16
+   {
+       SomePrimitiveTask2;
+   }
+
+   case 
+   {
+       SomePrimitiveTask3;
+   }
+}
+
+primitive task SomePrimitiveTask
+{
+    operator SomeOperator();
+}
+
+primitive task SomePrimitiveTask2
+{
+    operator SomeOperator2();
+}
+
+primitive task SomePrimitiveTask3
+{
+    operator SomeOperator3();
+}";
+
+            var maxN = 0;
+
+            Assert.AreEqual(true, BehaviorTestEngineRunner.RunMinimalInstance(text,
+                (n, message) =>
+                {
+                    maxN = n;
+
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual("Run SomeOperator", message);
+                            return true;
+
+                        case 2:
+                            Assert.AreEqual("Run SomeOperator", message);
+                            return false;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }));
+
+            Assert.AreEqual(2, maxN);
+        }
+
+        [Test]
+        [Parallelizable]
         public void CompoundHtnTaskCaseCondition_Case1()
         {
             var text = @"app PeaceKeeper

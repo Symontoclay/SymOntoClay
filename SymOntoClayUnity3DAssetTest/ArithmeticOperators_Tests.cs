aa6ca4608b159018fa99ecabe7b8aa4156cee310
metatypeman <metatypeman@gmail.com>
2025-10-18 15:59:46 +0300

    0.5.5 Tests for +=, -=, *=, /= operators

--- SymOntoClayUnity3DAssetTest/ArithmeticOperators_Tests.cs
+++ SymOntoClayUnity3DAssetTest/ArithmeticOperators_Tests.cs
@@ -738,5 +738,189 @@ private:
 
             Assert.AreEqual(3, maxN);
         }
+
+        [Test]
+        [Parallelizable]
+        public void AddAssign_Case1()
+        {
+            var text = @"app PeaceKeeper
+{
+    on Enter =>
+    {
+        'Begin' >> @>log;
+        SomeAutoProp += 2;
+        SomeAutoProp >> @>log;
+        'End' >> @>log;
+    }
+
+    prop SomeAutoProp: number = 16;
+}";
+
+            var maxN = 0;
+
+            Assert.AreEqual(true, BehaviorTestEngineRunner.RunMinimalInstance(text,
+                (n, message) =>
+                {
+                    maxN = n;
+
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual("Begin", message);
+                            return true;
+
+                        case 2:
+                            Assert.AreEqual("18", message);
+                            return true;
+
+                        case 3:
+                            Assert.AreEqual("End", message);
+                            return false;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }));
+
+            Assert.AreEqual(3, maxN);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void SubAssign_Case1()
+        {
+            var text = @"app PeaceKeeper
+{
+    on Enter =>
+    {
+        'Begin' >> @>log;
+        SomeAutoProp -= 2;
+        SomeAutoProp >> @>log;
+        'End' >> @>log;
+    }
+
+    prop SomeAutoProp: number = 16;
+}";
+
+            var maxN = 0;
+
+            Assert.AreEqual(true, BehaviorTestEngineRunner.RunMinimalInstance(text,
+                (n, message) =>
+                {
+                    maxN = n;
+
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual("Begin", message);
+                            return true;
+
+                        case 2:
+                            Assert.AreEqual("14", message);
+                            return true;
+
+                        case 3:
+                            Assert.AreEqual("End", message);
+                            return false;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }));
+
+            Assert.AreEqual(3, maxN);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void MulAssign_Case1()
+        {
+            var text = @"app PeaceKeeper
+{
+    on Enter =>
+    {
+        'Begin' >> @>log;
+        SomeAutoProp *= 2;
+        SomeAutoProp >> @>log;
+        'End' >> @>log;
+    }
+
+    prop SomeAutoProp: number = 16;
+}";
+
+            var maxN = 0;
+
+            Assert.AreEqual(true, BehaviorTestEngineRunner.RunMinimalInstance(text,
+                (n, message) =>
+                {
+                    maxN = n;
+
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual("Begin", message);
+                            return true;
+
+                        case 2:
+                            Assert.AreEqual("32", message);
+                            return true;
+
+                        case 3:
+                            Assert.AreEqual("End", message);
+                            return false;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }));
+
+            Assert.AreEqual(3, maxN);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void DivAssign_Case1()
+        {
+            var text = @"app PeaceKeeper
+{
+    on Enter =>
+    {
+        'Begin' >> @>log;
+        SomeAutoProp /= 2;
+        SomeAutoProp >> @>log;
+        'End' >> @>log;
+    }
+
+    prop SomeAutoProp: number = 16;
+}";
+
+            var maxN = 0;
+
+            Assert.AreEqual(true, BehaviorTestEngineRunner.RunMinimalInstance(text,
+                (n, message) =>
+                {
+                    maxN = n;
+
+                    switch (n)
+                    {
+                        case 1:
+                            Assert.AreEqual("Begin", message);
+                            return true;
+
+                        case 2:
+                            Assert.AreEqual("8", message);
+                            return true;
+
+                        case 3:
+                            Assert.AreEqual("End", message);
+                            return false;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(n), n, null);
+                    }
+                }));
+
+            Assert.AreEqual(3, maxN);
+        }
     }
 }

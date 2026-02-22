15aae7c95e1ae076c493427ebc65765df77ed605
metatypeman <metatypeman@gmail.com>
2026-02-22 13:11:30 +0200

    0.5.6 Monitor: refactoring

--- SymOntoClay.Monitor/Internal/FileWriter/FileWriterFactory.cs
+++ SymOntoClay.Monitor/Internal/FileWriter/FileWriterFactory.cs
@@ -1,4 +1,5 @@
 ﻿using SymOntoClay.CoreHelper.SerializerAdapters;
+using SymOntoClay.Monitor.Internal.FileWriter.General;
 using System;
 
 namespace SymOntoClay.Monitor.Internal.FileWriter
@@ -7,6 +8,12 @@ namespace SymOntoClay.Monitor.Internal.FileWriter
     {
         public static IMonitorFileWriter CreateMonitorFileWriter(KindOfSerialization kindOfSerialization, string messagesDir, string sessionName)
         {
+            switch(kindOfSerialization)
+            {
+                case KindOfSerialization.Json:
+                    return new MonitorFileWriter(messagesDir, sessionName);
+            }
+
             throw new NotImplementedException($"62A708F8-3DED-40DD-8D89-664328E49F80: {kindOfSerialization}");
         }
     }
--- SymOntoClay.Monitor/Internal/FileWriter/General/MonitorFileWriter.cs
+++ SymOntoClay.Monitor/Internal/FileWriter/General/MonitorFileWriter.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace SymOntoClay.Monitor.Internal.FileWriter.General
 {
-    internal class MonitorFileWriter
+    public class MonitorFileWriter : IMonitorFileWriter
     {
+#if DEBUG
+        private static readonly NLog.ILogger _globalLogger = NLog.LogManager.GetCurrentClassLogger();
+#endif
+
+        public MonitorFileWriter(string messagesDir, string sessionName)
+        {
+#if DEBUG
+            _globalLogger.Info($"messagesDir = {messagesDir}");
+            _globalLogger.Info($"sessionName = {sessionName}");
+#endif
+
+            throw new NotImplementedException("25B3E7BC-5C20-42EC-B2AD-88D1C8C0227C");
+        }
+
+        /// <inheritdoc/>
+        public string AbsoluteDirectoryName => throw new NotImplementedException("A50156D3-3795-4980-B9E8-A71424CDC9B6");
+
+        /// <inheritdoc/>
+        public IMonitorNodeFileWriter CreateMonitorNodeFileWriter(string nodeId)
+        {
+            throw new NotImplementedException("ADFAE250-9AA6-4219-BDBD-D2A5280F659F");
+        }
     }
 }

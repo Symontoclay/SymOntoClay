29719f951c05c418756ebf9753ada053a35e0e2a
metatypeman <metatypeman@gmail.com>
2026-03-15 11:25:47 +0200

    0.5.6 Monitor: refactoring

---
+++ SymOntoClay.Monitor.Common/Formats/IndexFileRowRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace SymOntoClay.Monitor.Common.Formats
+{
+    public readonly record struct IndexFileRowRecord(
+        string NodeId,
+        string ThreadId,
+        ulong MessageNumber,
+        ulong GlobalMessageNumber,
+        int KindOfMessage,
+        long StartPosition,
+        int DataLength);
+}

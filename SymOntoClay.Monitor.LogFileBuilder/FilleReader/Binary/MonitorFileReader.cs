eb54ddad5e92408b63540fcd0ffc66a6e3e53836
metatypeman <metatypeman@gmail.com>
2026-03-20 19:53:57 +0200

    0.5.6 Monitor: refactoring

--- SymOntoClay.Monitor.LogFileBuilder/FilleReader/Binary/MonitorFileReader.cs
+++ SymOntoClay.Monitor.LogFileBuilder/FilleReader/Binary/MonitorFileReader.cs
@@ -1,6 +1,8 @@
 ﻿using SymOntoClay.Monitor.Common.Data;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace SymOntoClay.Monitor.LogFileBuilder.FilleReader.Binary
 {
@@ -9,6 +11,8 @@ namespace SymOntoClay.Monitor.LogFileBuilder.FilleReader.Binary
 #if DEBUG
         private static readonly global::NLog.ILogger _globalLogger = global::NLog.LogManager.GetCurrentClassLogger();
 #endif
+
+        private const int _nodeLevel = 2;
 
         /// <inheritdoc/>
         public List<IndexFileRowRecord> GetIndexFileRowRecords(string targetDirectoryName, IEnumerable<KindOfMessage> targetKindOfMessages, IEnumerable<string> targetNodes, IEnumerable<string> targetThreads)
@@ -19,6 +23,8 @@ namespace SymOntoClay.Monitor.LogFileBuilder.FilleReader.Binary
 
             var result = new List<IndexFileRowRecord>();
 
+            FillUpFileRowRecords(ref result, targetDirectoryName, targetKindOfMessages, 1, targetNodes, targetThreads);
+
             throw new NotImplementedException("353BDAF9-AF2A-4C24-80CF-9DA637C46DCB");
         }
 
@@ -26,6 +32,44 @@ namespace SymOntoClay.Monitor.LogFileBuilder.FilleReader.Binary
         {
 #if DEBUG
             _globalLogger.Info($"targetDirectoryName = {targetDirectoryName}");
+            _globalLogger.Info($"levelNum = {levelNum}");
+            _globalLogger.Info($"targetNodes?.Count() = {targetNodes?.Count()}");
+#endif
+
+            switch (levelNum)
+            {
+                case _nodeLevel:
+                    if (targetNodes != null)
+                    {
+                        var directoryInfo = new DirectoryInfo(targetDirectoryName);
+
+#if DEBUG
+                        _globalLogger.Info($"directoryInfo.Name = {directoryInfo.Name}");
+#endif
+
+                        if (!targetNodes.Contains(directoryInfo.Name))
+                        {
+                            return;
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            var logFileName = Path.Combine(targetDirectoryName, "Logs.dat");
+
+#if DEBUG
+            _globalLogger.Info($"logFileName = {logFileName}");
+            _globalLogger.Info($"File.Exists(logFileName) = {File.Exists(logFileName)}");
+#endif
+
+            var indexFileName = Path.Combine(targetDirectoryName, "Logs.idx");
+
+#if DEBUG
+            _globalLogger.Info($"indexFileName = {indexFileName}");
+            _globalLogger.Info($"File.Exists(indexFileName) = {File.Exists(indexFileName)}");
 #endif
 
             throw new NotImplementedException("2893FD89-655C-4749-B8EB-A4CFF2D083E0");

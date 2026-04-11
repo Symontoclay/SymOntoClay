50cbcc4ea29cb66a8197f439a9e53c125d49d900
metatypeman <metatypeman@gmail.com>
2026-04-11 12:18:10 +0300

    0.5.6: added new options to the enum ComponentState

--- SymOntoClayUnityAssetCore/Internal/ComponentState.cs
+++ SymOntoClayUnityAssetCore/Internal/ComponentState.cs
@@ -20,18 +20,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace SymOntoClay.UnityAsset.Core.Internal
 {
     public enum ComponentState
     {
         Created,
+        LoadingFromSourceCode,
+        LoadingFromImage,
         Loaded,
         Started,
         Stopped,
+        SavingToImage,
         Died,
         Disposed
     }

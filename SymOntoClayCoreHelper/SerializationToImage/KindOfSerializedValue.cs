b7941a51ca0abf6b2a623c79aaa2a0ef02047cff
metatypeman <metatypeman@gmail.com>
2026-04-18 16:27:05 +0300

    0.5.6 serialization refactoring: SerializedValue

---
+++ SymOntoClayCoreHelper/SerializationToImage/KindOfSerializedValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymOntoClay.CoreHelper.SerializationToImage
+{
+    public enum KindOfSerializedValue
+    {
+        Null,
+        Literal,
+        ObjectPtr
+    }
+}
---
+++ SymOntoClayCoreHelper/SerializationToImage/SerializedValue.cs
@@ -0,0 +1,29 @@
+﻿namespace SymOntoClay.CoreHelper.SerializationToImage
+{
+    public class SerializedValue
+    {
+        public SerializedValue()
+        {
+        }
+
+        public SerializedValue(KindOfSerializedValue kindOfSerializedValue, long id, int typeId, string literal)
+        {
+            KindOfSerializedValue = kindOfSerializedValue;
+            Id = id;
+            TypeId = typeId;
+            Literal = literal;
+        }
+
+        public KindOfSerializedValue KindOfSerializedValue { get; private set; }
+        public long Id { get; private set; }
+        public int TypeId { get; private set; }
+        public string Literal { get; private set; }
+
+        //public override int GetHashCode()
+        //{
+        //    return Id.GetHashCode();
+        //}
+
+        //public override string ToString() => $"({nameof(Id)}: '{Id}', {nameof(TypeId)}: '{TypeId}')";
+    }
+}

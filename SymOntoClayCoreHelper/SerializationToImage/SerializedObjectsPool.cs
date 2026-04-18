8e0bc3c00182cc7c5bf1e23af20818c62dac5fe8
metatypeman <metatypeman@gmail.com>
2026-04-18 20:35:26 +0300

    0.5.6 SerializedObjectsPool

--- SymOntoClayCoreHelper/SerializationToImage/SerializedObjectsPool.cs
+++ SymOntoClayCoreHelper/SerializationToImage/SerializedObjectsPool.cs
@@ -1,19 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SymOntoClay.CoreHelper.SerializationToImage
 {
     public class SerializedObjectsPool: ISerializedObjectsPool
     {
+        private Dictionary<object, SerializedValue> _serializedObjects = new Dictionary<object, SerializedValue>();
+
         /// <inheritdoc/>
         public bool IsSerialized(object obj)
         {
-            throw new NotImplementedException("C8DD4FD5-C8A9-406A-9387-FBDFD0B5782E");
+            return _serializedObjects.ContainsKey(obj);
         }
 
         /// <inheritdoc/>
         public bool TryGetSerializedValue(object obj, out SerializedValue serializedValue)
         {
-            throw new NotImplementedException("C9B63C8E-1FA0-4F8D-A9A3-6B181700B088");
+            return _serializedObjects.TryGetValue(obj, out serializedValue);
         }
 
         /// <inheritdoc/>

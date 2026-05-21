9ec2b194f9fdcab4281c7beafd95cd0fca2a0ea4
metatypeman <metatypeman@gmail.com>
2026-05-21 21:01:54 +0300

    0.5.6 Pointer

--- SymOntoClayCoreHelper/Pointer.cs
+++ SymOntoClayCoreHelper/Pointer.cs
@@ -1,9 +1,19 @@
-﻿namespace SymOntoClay.CoreHelper
+﻿using SymOntoClay.Common.SerializationToImage.Attributes;
+
+namespace SymOntoClay.CoreHelper
 {
     public class Pointer<T>
     {
         public Pointer()
         {
         }
+
+        public Pointer(T value)
+        {
+            Value = value;
+        }
+
+        [SystemNoSerializedMember]
+        public T Value { get; set; }
     }
 }

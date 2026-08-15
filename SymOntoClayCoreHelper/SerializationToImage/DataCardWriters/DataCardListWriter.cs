cfa12c5b5e00bcfe464f376f7a8a50df43899e74
metatypeman <metatypeman@gmail.com>
2026-08-15 11:59:07 +0300

    0.5.6 DataCardListWriter

---
+++ SymOntoClayCoreHelper/SerializationToImage/DataCardWriters/DataCardListWriter.cs
@@ -0,0 +1,17 @@
+﻿using SymOntoClay.Common.Disposing;
+using System;
+
+namespace SymOntoClay.CoreHelper.SerializationToImage.DataCardWriters
+{
+    public class DataCardListWriter: Disposable, IDataCardWriter
+    {
+        /// <inheritdoc/>
+        public string RelativePath => string.Empty;
+
+        /// <inheritdoc/>
+        public void Write(IDataCard dataCard)
+        {
+            throw new NotImplementedException("C00B9E99-56CF-430F-B3CE-FB040BDA497D");
+        }
+    }
+}
